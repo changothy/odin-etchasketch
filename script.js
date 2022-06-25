b52992aca5b49a3f1bf88e4a8ada0717bc51dd49
changothy <timothychang.au@gmail.com>
2022-06-24 21:56:20 -0500

    Create 16x16 grid of divs

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const body = document.querySelector('body');
-const grid = document.createElement('div');
+const grid = document.querySelector('#grid');
 
-
-grid.setAttribute('id', 'grid');
-
-body.appendChild(grid);
+for (i = 0; i < 16; i++) {
+    const rowDiv = document.createElement('div');
+    let letter = String.fromCharCode(97 + i);
+    rowDiv.classList.add(`${letter}`);
+    grid.appendChild(rowDiv);
+    for (a = 0; a < 16; a++) {
+        const columnDiv = document.createElement('div');
+        columnDiv.classList.add(`${a}`);
+        // columnDiv.textContent = `${letter}${a}`
+        rowDiv.appendChild(columnDiv);
+    }
+}
