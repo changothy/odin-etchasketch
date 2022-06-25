1568d934b78145779c338c8a281c4c032eddcb2d
changothy <timothychang.au@gmail.com>
2022-06-24 22:59:23 -0500

    Add reset function, grid generate function
    Add reset function to reset grid and get user input to generate a new one.
    Provide some basic error handling to catch non-numeric values, negative values
    and values over 100.
    
    Put default grid drawing logic in its own function. Take an input of gridSize
    to draw the correct number of squares. Generate a grid of 16x16 by default.

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-const grid = document.querySelector('#grid');
+const DEFAULT_GRIDSIZE = 16;
 
-for (i = 0; i < 16; i++) {
-    const rowDiv = document.createElement('div');
-    let letter = String.fromCharCode(97 + i); // retrieve the character for a-p
-    rowDiv.classList.add(`${letter}`);
-    grid.appendChild(rowDiv);
+function reset(){
+    gridSize = prompt("Enter your preferred grid size (x by x):");
 
-    for (a = 0; a < 16; a++) { //for each row, add 16 column divs
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add(`${a}`);
-        columnDiv.classList.add("cell");
-        // columnDiv.textContent = `${letter}${a}`
-
-        columnDiv.addEventListener('mouseover', () => {
-            columnDiv.classList.toggle('paint');
-        });
-        rowDiv.appendChild(columnDiv);
+    if (gridSize > 100) {
+        alert("Maximum grid size is 100 x 100. Please enter a smaller number.");
+        reset();
+    } else if (gridSize < 0) {
+        alert("Please enter a number greater than 0.");
+        reset();
+    } else if (isNaN(gridSize)) {
+        alert("Please enter a valid value.");
+        reset();
+    } else {
+        generate(gridSize);
     }
 }
+
+function generate(gridSize){
+    const grid = document.querySelector('#grid');
+
+    // remove all child nodes before building
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    for (i = 0; i < gridSize; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add(`${i}`);
+        grid.appendChild(rowDiv);
+
+        for (a = 0; a < gridSize; a++) { //for each row, add 16 column divs
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add(`${a}`);
+            columnDiv.classList.add("cell");
+            // columnDiv.textContent = `${i}${a}`
+
+            columnDiv.addEventListener('mouseover', () => {
+                columnDiv.classList.add('paint');
+            });
+
+            // columnDiv.addEventListener('mouseenter', () => {
+            //     columnDiv.classList.add('paint');
+            // });
+
+            // columnDiv.addEventListener('mouseleave', () => {
+            //     columnDiv.classList.remove('paint');
+            // });
+
+            rowDiv.appendChild(columnDiv);
+        }
+    }
+}
+
+generate(DEFAULT_GRIDSIZE);
