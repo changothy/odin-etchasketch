976717ac04662c7ce3fb645f93a305f0016beeb7
changothy <timothychang.au@gmail.com>
2022-06-25 00:46:07 -0500

    Add rainbow mode and darkening colors as you draw

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const DEFAULT_GRIDSIZE = 16;
+let rainbowMode = false;
 
 // Reset the grid and remove all draw markings
 function reset() {
@@ -9,10 +10,25 @@ function reset() {
         for (c = 0; c < cells.length; c++) {
             let cell = cells[c];
             cell.classList.remove("paint"); //if it's on, remove it
+            cell.removeAttribute("style");
+            cell.style.filter = "brightness(110%)";
         }
     }
 }
 
+// TODO: get this working
+function toggleRainbowMode() {
+    (rainbowMode) ? rainbowMode = false : rainbowMode = true;
+}
+
+function getRandomRGB() {
+    let values = new Array(3);
+    for (i = 0; i < 3; i++) {
+        values[i] = Math.floor(Math.random() * 254) + 1;
+    }
+
+    return values;
+}
 function getUserGridSize() {
     gridSize = prompt("Enter your preferred grid size (x by x):");
 
@@ -28,6 +44,16 @@ function getUserGridSize() {
     } else {
         generate(gridSize);
     }
+}
+
+function darken(value) {
+    let currentBrightness = value.substring(value.indexOf("(") + 1, value.indexOf("%"));
+    let newBrightness = currentBrightness - 10;
+    if (newBrightness < 0) {
+        newBrightness = 0;
+    }
+
+    return "brightness(" + newBrightness + "%)";
 }
 
 function generate(gridSize) {
@@ -47,19 +73,30 @@ function generate(gridSize) {
             const rowDiv = document.createElement('div');
             rowDiv.classList.add(`${a}`);
             rowDiv.classList.add("cell");
+            rowDiv.style.filter = "brightness(110%)";
             // rowDiv.textContent = `${i}${a}`
 
             rowDiv.addEventListener('mouseover', () => {
+                // Paint with default color
+                // rowDiv.classList.add('paint');
+
+                // Paint with rainbow
+                // let values = getRandomRGB();
+                // rowDiv.setAttribute('style', `background: rgb(${values[0]}, ${values[1]}, ${values[2]})`);
+
+                // Gradually darken over time
                 rowDiv.classList.add('paint');
+                rowDiv.style.filter = darken(rowDiv.style.filter);
+
+                // Rough tracing
+                // rowDiv.addEventListener('mouseenter', () => {
+                //     rowDiv.classList.add('paint');
+                // });
+
+                // rowDiv.addEventListener('mouseleave', () => {
+                //     rowDiv.classList.remove('paint');
+                // });
             });
-
-            // rowDiv.addEventListener('mouseenter', () => {
-            //     rowDiv.classList.add('paint');
-            // });
-
-            // rowDiv.addEventListener('mouseleave', () => {
-            //     rowDiv.classList.remove('paint');
-            // });
 
             columnDiv.appendChild(rowDiv);
         }
